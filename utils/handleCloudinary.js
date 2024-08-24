f5a092b0f367917a6c3c3f1f159553e3ac0d847a
AbdElrahman-2004 <abdelrahman200410@gmail.com>
2024-08-24 09:58:20 +0300

    commit 16

--- utils/handleCloudinary.js
+++ utils/handleCloudinary.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
-const process = require("process");
 
 cloudinary.config({
-  cloud_name: "dlzlvlsvt",
-  api_key: "846924366384599",
-  api_secret: "nbcUsO39xICc531GE5sXSquH2Ck",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const uploadToCloudinary = async (files, folder) => {
